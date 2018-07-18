a0c0ce3820fe9c3b75a3875c9a5993bcac0e7e8f
Iain MacDonald <IJMacD@gmail.com>
2018-07-18 16:21:10 +0800

    Test Query without SDK wrapper where appropriate

--- test/expr.test.js
+++ test/expr.test.js
@@ -1,64 +1,62 @@
-require('fetch-everywhere');
-require('dotenv').config();
-const query = require('../query');
+const Query = require('../query');
 
 describe("Constants", () => {
     test("String", () => {
-        return query("SELECT 'hello'").then(r => {
+        return Query("SELECT 'hello'").then(r => {
             expect(r[1][0]).toBe("hello");
         });
     });
 
     test("Number", () => {
-        return query("SELECT 42").then(r => {
+        return Query("SELECT 42").then(r => {
             expect(r[1][0]).toBe(42);
         });
     });
 
     test("Negative Number", () => {
-        return query("SELECT -42").then(r => {
+        return Query("SELECT -42").then(r => {
             expect(r[1][0]).toBe(-42);
         });
     });
 
     test("Float Number", () => {
-        return query("SELECT 42.042042").then(r => {
+        return Query("SELECT 42.042042").then(r => {
             expect(r[1][0]).toBe(42.042042);
         });
     });
 
     test("Negative Float Number", () => {
-        return query("SELECT -42.042042").then(r => {
+        return Query("SELECT -42.042042").then(r => {
             expect(r[1][0]).toBe(-42.042042);
         });
     });
 
     test("Exp Float Number", () => {
-        return query("SELECT 42.042042e+8").then(r => {
+        return Query("SELECT 42.042042e+8").then(r => {
             expect(r[1][0]).toBe(4204204200);
         });
     });
 
     test("Negative Exp Float Number", () => {
-        return query("SELECT -42.042042e+8").then(r => {
+        return Query("SELECT -42.042042e+8").then(r => {
             expect(r[1][0]).toBe(-4204204200);
         });
     });
 
     test("Exp Negative Float Number", () => {
-        return query("SELECT 42.042042e-3").then(r => {
+        return Query("SELECT 42.042042e-3").then(r => {
             expect(r[1][0]).toBe(0.042042042);
         });
     });
 
     test("True", () => {
-        return query("SELECT true").then(r => {
+        return Query("SELECT true").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("False", () => {
-        return query("SELECT false").then(r => {
+        return Query("SELECT false").then(r => {
             expect(r[1][0]).toBe(false);
         });
     });
@@ -66,25 +64,25 @@ describe("Constants", () => {
 
 describe("Maths", () => {
     test("Addition", () => {
-        return query("SELECT 14 + 28").then(r => {
+        return Query("SELECT 14 + 28").then(r => {
             expect(r[1][0]).toBe(42);
         });
     });
     
     test("Subtraction", () => {
-        return query("SELECT 14 - 28").then(r => {
+        return Query("SELECT 14 - 28").then(r => {
             expect(r[1][0]).toBe(-14);
         });
     });
     
     test("Multiplication", () => {
-        return query("SELECT 14 * 28").then(r => {
+        return Query("SELECT 14 * 28").then(r => {
             expect(r[1][0]).toBe(14 * 28);
         });
     });
     
     test("Division", () => {
-        return query("SELECT 14 / 28").then(r => {
+        return Query("SELECT 14 / 28").then(r => {
             expect(r[1][0]).toBe(14 / 28);
         });
     });
@@ -92,7 +90,7 @@ describe("Maths", () => {
 
 describe("Strings", () => {
     test("Concat operator", () => {
-        return query("SELECT 'Hello, ' || 'world!'").then(r => {
+        return Query("SELECT 'Hello, ' || 'world!'").then(r => {
             expect(r[1][0]).toBe("Hello, world!");
         });
     });
@@ -100,80 +98,80 @@ describe("Strings", () => {
 
 describe("Functions", () => {
     test("WEEKDAY()", () => {
-        return query("SELECT WEEKDAY(2)").then(r => {
+        return Query("SELECT WEEKDAY(2)").then(r => {
             expect(r[1][0]).toBe("Tuesday");
         });
     });
 
     test("RAND()", () => {
-        return query("SELECT RAND()").then(r => {
+        return Query("SELECT RAND()").then(r => {
             expect(typeof r[1][0]).toBe("number");
         });
     });
 
     test("CONCAT()", () => {
-        return query("SELECT CONCAT('fat', 'her')").then(r => {
+        return Query("SELECT CONCAT('fat', 'her')").then(r => {
             expect(r[1][0]).toBe("father");
         });
     });
 
     test("CHAR()", () => {
-        return query("SELECT CHAR(65)").then(r => {
+        return Query("SELECT CHAR(65)").then(r => {
             expect(r[1][0]).toBe("A");
         });
     });
 
     test("UNICODE()", () => {
-        return query("SELECT UNICODE('a')").then(r => {
+        return Query("SELECT UNICODE('a')").then(r => {
             expect(r[1][0]).toBe(97);
         });
     });
 
     test("DATE()", () => {
-        return query("SELECT DATE('2018-07-18T14:27:29')").then(r => {
+        return Query("SELECT DATE('2018-07-18T14:27:29')").then(r => {
             expect(r[1][0]).toBe("2018-07-18");
         });
     });
 
     test("TIME()", () => {
-        return query("SELECT TIME('2018-07-18T14:27:29')").then(r => {
+        return Query("SELECT TIME('2018-07-18T14:27:29')").then(r => {
             expect(r[1][0]).toBe("14:27:29");
         });
     });
 
     test("DATETIME()", () => {
-        return query("SELECT DATETIME('2018-07-18T14:27:29')").then(r => {
+        return Query("SELECT DATETIME('2018-07-18T14:27:29')").then(r => {
             expect(r[1][0]).toBe('2018-07-18 14:27:29');
         });
     });
 
     describe("EXTRACT()", () => {
         test("EXTRACT(MILLENNIUM)", () => {
-            return query("SELECT EXTRACT(MILLENNIUM FROM '2018-07-18T14:27:29')").then(r=> {
+            return Query("SELECT EXTRACT(MILLENNIUM FROM '2018-07-18T14:27:29')").then(r=> {
                 expect(r[1][0]).toBe(3);
             });
         });
 
         test("EXTRACT(HOUR)", () => {
-            return query("SELECT EXTRACT(HOUR FROM '2018-07-18T14:27:29')").then(r=> {
+            return Query("SELECT EXTRACT(HOUR FROM '2018-07-18T14:27:29')").then(r=> {
                 expect(r[1][0]).toBe(14);
             });
         });
 
         test("EXTRACT(DOW)", () => {
-            return query("SELECT EXTRACT(DOW FROM '2018-07-18T14:27:29')").then(r=> {
+            return Query("SELECT EXTRACT(DOW FROM '2018-07-18T14:27:29')").then(r=> {
                 expect(r[1][0]).toBe(3);
             });
         });
 
         test("EXTRACT(DOY)", () => {
-            return query("SELECT EXTRACT(DOY FROM '2018-07-18T14:27:29')").then(r=> {
+            return Query("SELECT EXTRACT(DOY FROM '2018-07-18T14:27:29')").then(r=> {
                 expect(r[1][0]).toBe(199);
             });
         });
 
         test("EXTRACT(WEEK)", () => {
-            return query("SELECT EXTRACT(WEEK FROM '2018-07-18T14:27:29')").then(r=> {
+            return Query("SELECT EXTRACT(WEEK FROM '2018-07-18T14:27:29')").then(r=> {
                 expect(r[1][0]).toBe(29);
             });
         });
@@ -181,19 +179,19 @@ describe("Functions", () => {
 
     describe("CAST()", () => {
         test("INT", () => {
-            return query("SELECT CAST('42.547' AS INT)").then(r => {
+            return Query("SELECT CAST('42.547' AS INT)").then(r => {
                 expect(r[1][0]).toBe(42);
             });
         });
 
         test("FLOAT", () => {
-            return query("SELECT CAST('42.547' AS FLOAT)").then(r => {
+            return Query("SELECT CAST('42.547' AS FLOAT)").then(r => {
                 expect(r[1][0]).toBe(42.547);
             });
         });
 
         test("STRING", () => {
-            return query("SELECT CAST(42.547 AS STRING)").then(r => {
+            return Query("SELECT CAST(42.547 AS STRING)").then(r => {
                 expect(r[1][0]).toBe("42.547");
             });
         });
@@ -202,25 +200,25 @@ describe("Functions", () => {
 
 describe("Nested Functions", () => {
     test("Function -> Expression", () => {
-        return query("SELECT WEEKDAY(5 - 1)").then(r => {
+        return Query("SELECT WEEKDAY(5 - 1)").then(r => {
             expect(r[1][0]).toBe("Thursday");
         });
     });
 
     test("Function x2", () => {
-        return query("SELECT CONCAT(WEEKDAY(3), ' ', CHAR(66), ' ', UNICODE('d'))").then(r => {
+        return Query("SELECT CONCAT(WEEKDAY(3), ' ', CHAR(66), ' ', UNICODE('d'))").then(r => {
             expect(r[1][0]).toBe("Wednesday B 100");
         });
     });
 
     test("Function x4", () => {
-        return query("SELECT CHAR(UNICODE(CHAR(UNICODE('i'))))").then(r => {
+        return Query("SELECT CHAR(UNICODE(CHAR(UNICODE('i'))))").then(r => {
             expect(r[1][0]).toBe("i");
         });
     });
 
     test("(Function -> Expression) x2", () => {
-        return query("SELECT CHAR(UNICODE(CHAR(UNICODE('2') * 2)) + 3)").then(r => {
+        return Query("SELECT CHAR(UNICODE(CHAR(UNICODE('2') * 2)) + 3)").then(r => {
             expect(r[1][0]).toBe("g");
         });
     });
@@ -228,73 +226,73 @@ describe("Nested Functions", () => {
 
 describe("Logic Operators", () => {
     test("=", () => {
-        return query("SELECT 4 = 4").then(r => {
+        return Query("SELECT 4 = 4").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("!=", () => {
-        return query("SELECT 4 != 4").then(r => {
+        return Query("SELECT 4 != 4").then(r => {
             expect(r[1][0]).toBe(false);
         });
     });
 
     test("<", () => {
-        return query("SELECT 4 < 4").then(r => {
+        return Query("SELECT 4 < 4").then(r => {
             expect(r[1][0]).toBe(false);
         });
     });
 
     test(">", () => {
-        return query("SELECT 4 > 4").then(r => {
+        return Query("SELECT 4 > 4").then(r => {
             expect(r[1][0]).toBe(false);
         });
     });
 
     test("<=", () => {
-        return query("SELECT 4 <= 4").then(r => {
+        return Query("SELECT 4 <= 4").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test(">", () => {
-        return query("SELECT 4 >= 4").then(r => {
+        return Query("SELECT 4 >= 4").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("IS NULL", () => {
-        return query("SELECT null IS NULL").then(r => {
+        return Query("SELECT null IS NULL").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("IS NOT NULL", () => {
-        return query("SELECT 42 IS NOT NULL").then(r => {
+        return Query("SELECT 42 IS NOT NULL").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("LIKE", () => {
-        return query("SELECT 'hello' LIKE 'h?l%'").then(r => {
+        return Query("SELECT 'hello' LIKE 'h?l%'").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("NOT LIKE", () => {
-        return query("SELECT 'goodbye' NOT LIKE 'h?l%'").then(r => {
+        return Query("SELECT 'goodbye' NOT LIKE 'h?l%'").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("REGEXP", () => {
-        return query("SELECT 'abcdef' REGEXP '[a-z]{6}'").then(r => {
+        return Query("SELECT 'abcdef' REGEXP '[a-z]{6}'").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });
 
     test("NOT REGEXP", () => {
-        return query("SELECT 'abcdef' NOT REGEXP '^[0-9]+'").then(r => {
+        return Query("SELECT 'abcdef' NOT REGEXP '^[0-9]+'").then(r => {
             expect(r[1][0]).toBe(true);
         });
     });

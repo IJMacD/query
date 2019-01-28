72cb6d8a5633efc2e23ad8113317a587399cce5c
Iain MacDonald <iain.ilearner@gmail.com>
2019-01-28 12:15:12 +0800

    Ability to reference column aliases in expressions

--- test/lang.test.js
+++ test/lang.test.js
@@ -20,9 +20,21 @@ test("SELECT selects columns", () => {
     });
 });
 
-test("Column Alias", () => {
-    return demoQuery("SELECT 'hello' AS greeting").then(r => {
-        expect(r[0][0]).toBe("greeting");
+describe("Column Alias", () => {
+    test("Simple Alias", () => {
+        return demoQuery("SELECT 'hello' AS greeting").then(r => {
+            expect(r[0][0]).toBe("greeting");
+            expect(r[1][0]).toBe("hello");
+        });
+    });
+
+    test("Alias Reference", () => {
+        return demoQuery("SELECT 9 AS num, num + num AS num2").then(r => {
+            expect(r[0][0]).toBe("num");
+            expect(r[1][0]).toBe(9);
+            expect(r[0][1]).toBe("num2");
+            expect(r[1][1]).toBe(18);
+        });
     });
 });
 

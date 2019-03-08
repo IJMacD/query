da18fde3c9f5d201e7429f292dc82c34a62a4dc9
Iain MacDonald <IJMacD@gmail.com>
2019-03-08 16:26:37 +0800

    Make sure limits are made of constant expressions

--- src/limit.js
+++ src/limit.js
@@ -1,10 +1,16 @@
-const { evaluateConstantExpression } = require('./evaluate');
+const { evaluateConstantExpression, isConstantExpression } = require('./evaluate');
 
 module.exports = {
     applyLimit,
 };
 
 function applyLimit(rows, limit, offset) {
+    if (typeof limit !== "undefined" && !isConstantExpression(limit)) {
+        throw new Error(`LIMIT must be a constant expression: ${limit.source}`);
+    }
+    if (typeof offset !== "undefined" && !isConstantExpression(offset)) {
+        throw new Error(`OFFSET must be a constant expression: ${offset.source}`);
+    }
     const start = offset ? evaluateConstantExpression(offset) : 0;
     const end = limit ? (start + evaluateConstantExpression(limit)) : rows.length;
     return rows.slice(start, end);
